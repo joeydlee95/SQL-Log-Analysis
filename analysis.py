59333bf890bf6d8382e6692ff41607b9b12db5bd
joeydlee95 <joeydonglee@gmail.com>
2017-09-14 14:23:14 -0700

    function for popular author

--- analysis.py
+++ analysis.py
@@ -1,11 +1,22 @@
 import analysisDB
 
-# Gets the most viewed article (article name, views)
+# Gets the most viewed article ([Article name], [Number of views]).
 posts = analysisDB.get_mostviewed()
-
-# Gets the article name and number of views of that article
+# Checks if there is anything in posts
 if str(posts[0]) == "None": 
     print("There were no articles that got views.\n")
 else:
-  print("\"" + str(posts[0]) + "\" - " + str(posts[1]))
+    print("\"" + str(posts[0]) + "\" - " + str(posts[1]) + " views")
+
+
+# Gets the most popular author ([Author's name], [Number of views]).
+pop_author = analysisDB.get_popular_author()
+# Checks if there is anything in pop_author
+if str(pop_author[0]) == "None":
+    print("There is were no authors that got views.\n")
+else:
+    print(str(pop_author[0]) + " - " + str(pop_author[1]) + " views")
+
+
+
 
--- analysisDB.py
+++ analysisDB.py
@@ -12,7 +12,7 @@ def get_mostviewed():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
 
-    # Gets (title, total views)
+    # Gets (title, total views) of the most viewed article
     c.execute("select title, count(path) as views \
                from log left join articles \
                on log.path LIKE '%' || articles.slug \
@@ -24,8 +24,22 @@ def get_mostviewed():
     db.close()
     return posts
 
-def popular_author():
+def get_popular_author():
     """Gets the row with an author and number of views."""
-    db = psychopg2.connect(database=DBNAME)
-    c = db.cursor("select author, count(author) from ;")
-    c.execute()
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    # Join articles and authors to include the author's name
+    # then count the number of views each author gets. 
+    c.execute("select name, count(name) as views \
+               from (select slug, name \
+               from articles join authors \
+               on articles.author = authors.id) as most_viewed \
+               join log \
+               on log.path LIKE '%' || most_viewed.slug \
+               where log.path LIKE '/article/%' \
+               group by most_viewed.name \
+               order by views desc \
+               limit 1;")
+    posts = c.fetchone()
+    db.close()
+    return posts
