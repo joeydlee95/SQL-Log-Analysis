d49e7b45e7c0626780e94ba23ff08a008f40895d
joeydlee95 <joeydonglee@gmail.com>
2017-09-19 15:40:39 -0700

    fixes for commented errors

--- analysis.py
+++ analysis.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python
+#
+# Joey Lee
+
+
 import analysisDB
 import datetime
+
 
 # Gets the most viewed article ([Article name], [Number of views]).
 posts = analysisDB.get_mostviewed()
@@ -7,20 +13,23 @@ posts = analysisDB.get_mostviewed()
 if str(posts[0]) == "None":
     print("There were no articles that got views.\n")
 else:
-    print("\"" + str(posts[0]) + "\" - " + str(posts[1]) + " views")
+    for post in posts:
+        print("\"" + str(post[0]) + "\" - " + str(post[1]) + " views")
 
 
 # Gets the most popular author ([Author's name], [Number of views]).
-pop_author = analysisDB.get_popular_author()
+pop_authors = analysisDB.get_popular_author()
 # Checks if there is anything in pop_author
-if str(pop_author[0]) == "None":
+if str(pop_authors[0]) == "None":
     print("There is were no authors that got views.\n")
 else:
-    print(str(pop_author[0]) + " - " + str(pop_author[1]) + " views")
+    for pop_author in pop_authors:
+        print(str(pop_author[0]) + " - " + str(pop_author[1]) + " views")
 
 # Gets the days that had more than 1% of requests lead to errors.
 error_days = analysisDB.get_percent_error()
+if str(error_days[0]) == "None":
+    print("There were no days that had more that 1% errors.\n")
 # Changes number formatted date to (month day, year) format.
-error_date = error_days[0].strftime("%B %d, %Y")
-error_percent = error_days[1] * 100
-print(error_date + " - " + str(round(error_percent, 1)) + "% errors")
+for error_day in error_days:
+    print('{0:%B %d, %Y} - {1:.1%} errors'.format(error_day[0], error_day[1]))
--- analysisDB.py
+++ analysisDB.py
@@ -13,14 +13,13 @@ def get_mostviewed():
     c = db.cursor()
 
     # Gets (title, total views) of the most viewed article
-    c.execute("select title, count(path) as views \
-               from log left join articles \
-               on log.path LIKE '%' || articles.slug \
-               where log.path LIKE '/article/%' \
-               group by title \
-               order by views desc \
-               limit 1;")
-    posts = c.fetchone()
+    c.execute("""SELECT title, count(path) AS views
+               FROM articles LEFT JOIN log
+               ON log.path = '/article/' || articles.slug
+               GROUP BY title
+               ORDER BY views DESC
+               LIMIT 3;""")
+    posts = c.fetchall()
     db.close()
     return posts
 
@@ -31,17 +30,15 @@ def get_popular_author():
     c = db.cursor()
     # Join articles and authors to include the author's name
     # then count the number of views each author gets.
-    c.execute("select name, count(name) as views \
-               from (select slug, name \
-               from articles join authors \
-               on articles.author = authors.id) as most_viewed \
-               join log \
-               on log.path LIKE '%' || most_viewed.slug \
-               where log.path LIKE '/article/%' \
-               group by most_viewed.name \
-               order by views desc \
-               limit 1;")
-    posts = c.fetchone()
+    c.execute("""SELECT name, count(name) AS views
+               FROM (SELECT slug, name
+               FROM articles JOIN authors
+               ON articles.author = authors.id) AS most_viewed
+               JOIN log
+               ON log.path = '/article/' || most_viewed.slug
+               GROUP BY most_viewed.name
+               ORDER BY views DESC;""")
+    posts = c.fetchall()
     db.close()
     return posts
 
@@ -62,11 +59,11 @@ def get_percent_error():
                    where status != '200 OK' \
                    group by date(time);")
     # Joins the two views and gets the percent of error requests.
-    c.execute("select total_views.date, \
-               round(error_count/total_count::numeric, 3) as percent_error \
-               from total_views join error_views \
-               on total_views.date = error_views.date \
-               where round(error_count/total_count::numeric, 3) > 0.01;")
-    posts = c.fetchone()
+    c.execute("""SELECT total_views.date,
+               round(error_count/total_count::numeric, 3) AS percent_error
+               FROM total_views JOIN error_views
+               ON total_views.date = error_views.date
+               WHERE round(error_count/total_count::numeric, 3) > 0.01;""")
+    posts = c.fetchall()
     db.close()
     return posts
